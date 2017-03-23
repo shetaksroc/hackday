21458aff4f8f52b3a312f088c0cf722d9814ffb4
akshayshet <akshay@boomerangcommerce.com>
2017-03-23 20:11:04 +0530

    added the string replacement for data when the () are present

--- scripts/read_json_map.py
+++ scripts/read_json_map.py
@@ -1,6 +1,7 @@
 import numpy as np
 import csv
 import json,sys
+import re
 txt = open(sys.argv[1]);
 data_in_string_format=txt.read()
 data_list=json.loads(data_in_string_format)
@@ -8,7 +9,9 @@ final_list=[]
 for row in data_list:
 	row_list=[]
 	for column in row:
-		row_list.append(row[column])
+		column_value=str(row[column])
+		replaced = re.sub('\(.*\)', '', column_value)
+		row_list.append(replaced)
 	final_list.append(row_list)	
 #print final_list
 
@@ -17,11 +20,16 @@ def createDistinctAttributes(final_list):
 	merch_l1=set()
 	merch_l2=set()
 	merch_l3=set()
+	distinct_attributes=dict()
 	for row in final_list:
 		brands.add(row[2])
 		merch_l1.add(row[4])
 		merch_l2.add(row[5])
 		merch_l3.add(row[6])
+	distinct_attributes['brands']=brands
+	distinct_attributes['merch_l1']=merch_l1
+	distinct_attributes['merch_l2']=merch_l2
+	distinct_attributes['merch_l3']=merch_l3	
 	return list(brands,merch_l1,merch_l2,merch_l3)	
 
 #print brands
